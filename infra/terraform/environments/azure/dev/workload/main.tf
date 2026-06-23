bb0fca71f2bc76eaf1cf9b1632acf4db415a7229
Saul Patino Jr <saul.patino@cbts.com>
2026-06-23 03:54:53 -0500

    fix: Update workload remote state key to match pipeline configuration

--- infra/terraform/environments/azure/dev/workload/main.tf
+++ infra/terraform/environments/azure/dev/workload/main.tf
@@ -4,7 +4,7 @@ data "terraform_remote_state" "platform" {
     resource_group_name  = var.tfstate_resource_group
     storage_account_name = var.tfstate_storage_account
     container_name       = var.tfstate_container
-    key                  = "platform.tfstate"
+    key                  = "dev.terraform.tfstate"
   }
 }
 
